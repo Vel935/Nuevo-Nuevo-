8f0c236cb5e93706ff260e9b75656cac6504f6fb
Vel935 <103478887+Vel935@users.noreply.github.com>
2023-09-19 18:18:16 -0500

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <center>
+      <h1>Bienvenidos a la app de tareas</h1>
+      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+
+      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
+<br>
+
+<br>
+      <Login/>
+    
+      
+    </center>
+    
+
+  </div>
+</template>
+<script>
+
+definePageMeta({
+  layout: "blank",
+});
+
+import Login from "~/components/tasks/login.vue";
+import Formulario from "~/components/tasks/login.vue";
+
+export default {
+  components: {
+    Formulario,
+    Login
+},
+  head: {
+    title: 'Formulario de Ejemplo' // Puedes personalizar el título de la página aquí
+  }
+};
+
+
+</script>
